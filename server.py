7d8bb5306ee50cbec0faca7dfdbd4e83757e21d9
Isaac Ormesher <mathsgeekjr@gmail.com>
2019-04-11 21:29:21 +0100

    Added definition

---
+++ server.py
@@ -0,0 +1,28 @@
+#Server
+from flask import Flask, request, render_template
+from solver import minimise_dictionary, get_best_words, get_definition
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    words = []
+<<<<<<< HEAD
+    definition = ''
+    if "inputText" in request.args:
+        letters = sorted(list(request.args["inputText"].lower()))
+        dictionary = minimise_dictionary(letters)
+        words = get_best_words(dictionary)
+        definition = get_definition(words[0])
+    return render_template("index.html", words=words, definition=definition)
+=======
+    inputText = request.args.get("inputText", "")
+    if inputText:
+        letters = sorted(list(inputText.lower()))
+        dictionary = minimise_dictionary(letters)
+        words = get_best_words(dictionary)       
+    return render_template("index.html", words=words, inputText=inputText)
+>>>>>>> ce9259d4ab40777755fbbe9fe2f41ea4616d19c6
+
+if __name__ == '__main__':
+    app.run(debug=True)
