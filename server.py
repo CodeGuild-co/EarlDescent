72c791a3e2157f3fe1551fa94caf0866ad083e02
Isaac Ormesher <mathsgeekjr@gmail.com>
2019-04-11 21:22:31 +0100

    Added definition for the best word

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 #Server
 from flask import Flask, request, render_template
-from solver import minimise_dictionary, get_best_words
+from solver import minimise_dictionary, get_best_words, get_definition
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     words = []
+    definition = ''
     if "inputText" in request.args:
         letters = sorted(list(request.args["inputText"].lower()))
         dictionary = minimise_dictionary(letters)
-        words = get_best_words(dictionary)       
-    return render_template("index.html", words=words)
+        words = get_best_words(dictionary)
+        definition = get_definition(words[0])
+    return render_template("index.html", words=words, definition=definition)
 
 if __name__ == '__main__':
     app.run(debug=True)
